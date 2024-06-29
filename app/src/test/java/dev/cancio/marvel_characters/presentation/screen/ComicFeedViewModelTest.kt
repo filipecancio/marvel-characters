bb709dfc9aaf05eae7d1784c5c1ecd4022a70973
cancio <filipe.cancio@gmail.com>
2024-06-29 16:21:24 -0300

    :construction: update test version

---
+++ app/src/test/java/dev/cancio/marvel_characters/presentation/screen/ComicFeedViewModelTest.kt
@@ -0,0 +1,11 @@
+package dev.cancio.marvel_characters.presentation.screen
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ComicFeedViewModelTest {
+    @Test
+    fun `WHEN onCreate THEN show a Comic list`(){
+        assertEquals(4, 2 + 2)
+    }
+}
